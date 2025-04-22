885ccb1bd91c11314986b2969dc3f205fb971a41
Sanja Jovičić <119461156+Sanja24It@users.noreply.github.com>
2025-04-22 17:20:21 +0200

    script.js

--- insure-landing-page-master/script.js
+++ insure-landing-page-master/script.js
@@ -1,17 +1,24 @@
 const btn_mobile = document.querySelector(".menu_ham");
-const nav = document.querySelector(".nav")
+const nav = document.querySelector(".nav");
 
+window.addEventListener("resize", () =>{
+    if(window.innerWidth > 768){
+      nav.style.display = "block";
+    } else{
+      nav.style.display = "none";
+    }
+})
 
 btn_mobile.addEventListener("click", function(){
- 
-
+  
    if(btn_mobile.hasAttribute("data-open")){
-    btn_mobile.removeAttribute("data-open")
-    btn_mobile.setAttribute("data-close","close")
-    nav.style.display = "block"
+    btn_mobile.removeAttribute("data-open");
+    btn_mobile.setAttribute("data-close","close");
+    nav.style.display = "block";
    } else{
-    btn_mobile.removeAttribute("data-close")
-    btn_mobile.setAttribute("data-open","open")
-    nav.style.display = "none"
+    btn_mobile.removeAttribute("data-close");
+    btn_mobile.setAttribute("data-open","open");
+    nav.style.display = "none";
    }
 })
+
